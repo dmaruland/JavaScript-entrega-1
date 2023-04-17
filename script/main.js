45d1602296ae65cb3a6828d22c7cc5d8982152aa
dmaruland <123033734+dmaruland@users.noreply.github.com>
2023-04-17 17:04:57 -0500

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,118 +1,153 @@
+// Entrega 2 -->> Cotización con condición de comisión, ciclo, Objeto, Arrays, TRM Variable y función de orden superior para filtrar la mayoría de edad
 
-// Ejemplo 1: Cotización simple sin condición de descuento
-/*function multiplicarNumeros(solicitud,TRM){
-  
-let resultado = solicitud * TRM;
-return resultado;
-    
+function mayorQue(n) {
+    return(m) => m > n;
 }
-let solicitud = parseInt(prompt("Ingresa el valor de USD que quieres vender por COP "));
-let TRM = 4500;
-let resultado = multiplicarNumeros(solicitud, TRM);
-let comision = resultado * 0.1;
-let total = resultado + comision;
-multiplicarNumeros(solicitud,TRM); 
-console.log("La cotización es: " + solicitud*TRM);
-console.log("La comisión es de " + comision);
-alert("El total a pagar incluyendo comisión es de " + total);*/
 
-// Ejemplo 2: Agregar descuento por ventas superiores a 10.000 USD
-    /*function multiplicarNumeros(solicitud,TRM){
-  
-    let resultado = solicitud * TRM;
-    return resultado;
-        
-    }
-    let solicitud = parseInt(prompt("Ingresa el valor de USD que quieres vender por COP "));
-    let fee1 = 0.1
-    let fee2 = 0.05;
-    let TRM = 4500;
-    let resultado = multiplicarNumeros(solicitud, TRM);
-    let comision = resultado * fee2;
-    if (solicitud > 10000) {
-        comision = resultado * fee2;
-        console.log(" Puedes acceder a un descuento en la comisión, tu comisión es " + fee2)
-        alert("Has accedido a un descuento en la comisión.  Pasó del 10% al 5%")
-    } else{
-        comision = resultado * fee1;
-        console.log("Solo ventas superiores a 10.000 USD acceden a descuento en comisión")
-        alert(" Vende más de 10.000 USD y accede a un descuento del 50% en la comisión")
-    }
-    let total = resultado + comision;
-    multiplicarNumeros(solicitud,TRM); 
-    console.log("La cotización es: " + solicitud*TRM);
-    console.log("La comisión es de " + comision);
-    console.log("El total en COP es de: " + total);
-    alert("El total a pagar en pesos incluyendo comisión es de: " + total); */
-    
+let mayorDeEdad = mayorQue(17); 
 
-// Ejemplo 3: Cotización con condición de comisión y ciclo
-function multiplicarNumeros(solicitud,TRM){
+let numero = parseInt(prompt(" Qué edad tiene la persona? "));
+let resultadoEdad = mayorDeEdad(numero);
+
+if (resultadoEdad) {
+    console.log("El usuario declaró tener " + numero + " años");
+    function multiplicarNumeros(solicitud,TRM){
   
-    let resultado = solicitud * TRM;
-    return resultado;
-        
-    }
-    
-    let solicitud = parseInt(prompt("Ingresa el valor de BTC que quieres vender por USD "));
-    let fee1 = 0.02;
-    let fee2 = 0.01;
-    let TRM = 28500;
-    let resultado = multiplicarNumeros(solicitud, TRM);
-    let comision = resultado * fee2;
-    if (solicitud > 5) {
-        comision = resultado * fee2;
-        console.log(" Puedes obtener un descuento en la comisión, tu comisión es " + fee2)
-        alert("Has obtenido un descuento en la comisión.  Pasó del 2% al 1%")
-    } else{
-        comision = resultado * fee1;
-        console.log("Solo ventas superiores a 5 BTC obtienen un descuento en la comisión")
-        alert(" Vende más de 5 BTC y obtén un descuento del 50% en la comisión")
-    }
-    let total = resultado + comision;
-    multiplicarNumeros(solicitud,TRM); 
-    console.log("La cotización es: " + solicitud*TRM);
-    console.log("La comisión es de " + comision);
-    console.log("El total en USD es de: " + total);
-    alert("El total a pagar en USD incluyendo comisión es de: " + total);
-    
-   let repetir = prompt("¿Desea otra cotización? Responda Si o No");
-   if (repetir.toUpperCase() == "NO") {
-    alert("Gracias por cotizar con nosotros");}
-    else{
-        while (repetir.toUpperCase() == "SI"){ 
-        function multiplicarNumeros(solicitud,TRM){
-  
-            let resultado = solicitud * TRM;
-            return resultado;
-                
-            }
+        let resultado = solicitud * TRM;
+        return resultado;
             
-            let solicitud = parseInt(prompt("Ingresa el valor de BTC que quieres vender por USD "));
-            let fee1 = 0.02;
-            let fee2 = 0.01;
-            let TRM = 28500;
-            let resultado = multiplicarNumeros(solicitud, TRM);
-            let comision = resultado * fee2;
-            if (solicitud > 5) {
+        }
+        const TASA = [USD = 30500, COP = 150000000, PEN = 114000, CLP = 24050000];
+        let tasaAplicada = TASA[
+        prompt("¿Cuál moneda quieres intercambiar por BTC? Escribe: 0 para USD, 1 para COP, 2 para PEN o 3 para CLP")
+        ]
+        console.log("La tasa de intercambio aplicada es " + tasaAplicada);
+    
+        let solicitud = parseInt(prompt("Ingresa la cantidad de BTC que quieres vender" ));
+        let fee1 = 0.02;
+        let fee2 = 0.01;
+        let TRM = tasaAplicada;
+        let resultado = multiplicarNumeros(solicitud, TRM);
+        let comision = resultado * fee2;
+        
+        if (solicitud > 5) {
                 comision = resultado * fee2;
                 console.log(" Puedes obtener un descuento en la comisión, tu comisión es " + fee2)
                 alert("Has obtenido un descuento en la comisión.  Pasó del 2% al 1%")
             } else{
                 comision = resultado * fee1;
-                console.log("Solo ventas superiores a 5 BTC acceden a descuento en comisión")
-                alert(" Vende más de 5 BTC y accede a un descuento del 50% en la comisión")
+                console.log("Solo ventas superiores a 5 BTC obtienen un descuento en la comisión")
+                alert(" Vende más de 5 BTC y obtén un descuento del 50% en la comisión")
             }
             let total = resultado + comision;
             multiplicarNumeros(solicitud,TRM); 
             console.log("La cotización es: " + solicitud*TRM);
             console.log("La comisión es de " + comision);
             console.log("El total en USD es de: " + total);
-            alert("El total a pagar en USD incluyendo comisión es de: " + total);
-            repetir = prompt("¿Desea otra cotización? Responda Si o No");
-            if (repetir.toUpperCase() == "NO") {
-                alert("Gracias por cotizar con nosotros");
-                endLoop;
-                
+            alert("El total a pagar en la moneda seleccionada incluyendo comisión es de: " + total);
+            
+           let repetir = prompt("¿Desea otra cotización? Responda Si o No");
+           
+           if (repetir.toUpperCase() == "NO") {
+            let deseaCotizacion = prompt("¿Quieres enviar la cotización a tu correo? responde Si o No");
+                    
+            if (deseaCotizacion.toUpperCase() == "SI") {
+    
+    
+            let unaCotizacion = {
+                nombres:"",
+                apellidos:"",
+                cedula:"",
+                email:"",
+                total: total
+    
+             }
+    
+            unaCotizacion.nombres = prompt("¿Cuáles son tus nombres?");
+            unaCotizacion.apellidos = prompt("¿Cuáles son tus apellidos?");
+            unaCotizacion.cedula = prompt("¿Cuál es tu cédula?");
+            unaCotizacion.email = prompt("¿Cuál es tu e-mail?");
+    
+            console.log("Los datos de la cotización son ", unaCotizacion);
+            let fechaCotizacion = new Date();
+            alert("Correo enviado con cotización para venta de BTC por valor total de " + total + " con fecha: " + fechaCotizacion + " tienes 24 horas para ejecutar la transacción");
+    
+            } else {
+            alert("Gracias por cotizar con nosotros");
+    }
+            }
+            else{
+                while (repetir.toUpperCase() == "SI"){ 
+                function multiplicarNumeros(solicitud,TRM){
+          
+                    let resultado = solicitud * TRM;
+                    return resultado;
+                        
+                    }
+                    
+                    const TASA = [USD = 30500, COP = 150000000, PEN = 114000, CLP = 24050000];
+                        let tasaAplicada = TASA[
+                        prompt("¿Cuál moneda quieres intercambiar por BTC? Escribe: 0 para USD, 1 para COP, 2 para PEN o 3 para CLP")
+                         ]
+                    console.log("La tasa de intercambio aplicada es " + tasaAplicada);
+    
+                    let solicitud = parseInt(prompt("Ingresa la cantidad de BTC que quieres vender" ));
+                    let fee1 = 0.02;
+                    let fee2 = 0.01;
+                    let TRM = tasaAplicada;
+                    let resultado = multiplicarNumeros(solicitud, TRM);
+                    let comision = resultado * fee2;
+                    if (solicitud > 5) {
+                        comision = resultado * fee2;
+                        console.log(" Puedes obtener un descuento en la comisión, tu comisión es " + fee2)
+                        alert("Has obtenido un descuento en la comisión.  Pasó del 2% al 1%")
+                    } else{
+                        comision = resultado * fee1;
+                        console.log("Solo ventas superiores a 5 BTC acceden a descuento en comisión")
+                        alert(" Vende más de 5 BTC y accede a un descuento del 50% en la comisión")
+                    }
+                    let total = resultado + comision;
+                    multiplicarNumeros(solicitud,TRM); 
+                    console.log("La cotización es: " + solicitud*TRM);
+                    console.log("La comisión es de " + comision);
+                    console.log("El total en la mineda seleccionada es de: " + total);
+                    alert("El total a pagar en la moneda seleccionada incluyendo comisión es de: " + total);
+                    repetir = prompt("¿Desea otra cotización? Responda Si o No");
+                    if (repetir.toUpperCase() == "NO") {
+                        let deseaCotizacion = prompt("¿Quieres enviar la última cotización a tu correo? responde Si o No");
+                    
+                        if (deseaCotizacion.toUpperCase() == "SI") {
+    
+    
+                        let unaCotizacion = {
+                        nombres:"",
+                        apellidos:"",
+                        cedula:"",
+                        email:"",
+                        total: total,
+                    
+                        }
+    
+                        unaCotizacion.nombres = prompt("¿Cuáles son tus nombres?");
+                        unaCotizacion.apellidos = prompt("¿Cuáles son tus apellidos?");
+                        unaCotizacion.cedula = prompt("¿Cuál es tu cédula?");
+                        unaCotizacion.email = prompt("¿Cuál es tu e-mail?");
+                        
+                        
+                        console.log("Los datos de la cotización son ", unaCotizacion);
+                        let fechaCotizacion = new Date();
+                        alert("Correo enviado con cotización para venta de BTC por valor total de " + total + " con fecha: " + fechaCotizacion + " tienes 24 horas para ejecutar la transacción");
+    
+                     } else {
+                     alert("Gracias por cotizar con nosotros");
+                           
+                        endLoop;
+                        
+                    }}
+    
+                   
             }}
-    } 
+} else {
+    alert("Debes ser mayor de edad para vender en nuestro sitio ")
+    
+}
