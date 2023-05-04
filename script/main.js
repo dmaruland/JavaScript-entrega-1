aa3f6ae8ac0a7afc53cd9bce2e79e13723fef65a
dmaruland <123033734+dmaruland@users.noreply.github.com>
2023-05-03 19:19:11 -0500

    Add files via upload
    Actualización con integración al HTML

--- script/main.js
+++ script/main.js
@@ -1,206 +1,160 @@
 
-// Re - Entrega 2 completa: nuevas funciones para divisas (línea 23-43), recolectar información para envío de correo y almacenamiento para formato csv (línea 76-99).  Se utiliza el método map para crear el nuevo Array de monedas disponibles (línea 36-37) y se agregan las variables a la consola y las alertas.
+// Re - Entrega 3: Se cambian los inputs de prompts a casillas de texto en el HTML. Esta nueva presentación permite multiples cotizaciones sin tener que usar ciclos.  Hay elementos del HTML que aparecen y desaparecen en función de lo que el usuario va ingresando.  
 
-function mayorQue(n) {
-    return(m) => m > n;
+// Esconder el simulador cuando se cargue la página y función para verificación de edad
+
+window.addEventListener('load', function() {
+  const mostrarSimulador = document.getElementById('simulador');
+  mostrarSimulador.style.display.none;
+});
+
+
+
+function verificar(){
+  let edadUsuario;
+  edadUsuario=Number(document.getElementById("edad").value);
+  if(edadUsuario>17){
+      const mostrarSimulador = document.getElementById('simulador');
+      mostrarSimulador.style.display = 'block'
+      document.getElementById("mensajeMenorEdad").hidden = true
+      alert("El usuario es mayor de edad y ha declarado tener: " + edadUsuario + " años");
+
+  }else{
+      const mostrarSimulador = document.getElementById('simulador');    
+      mostrarSimulador.style.display.none
+      alert("Debes ser mayor de edad para vender en nuestro sitio ")
+              
+      }
+  }
+
+function exchange() {
+  const cantidad = parseFloat(document.getElementById("cantidad").value);
+  const from = document.getElementById("from").value;
+  const to = document.getElementById("to").value;
+  localStorage.setItem("from", from);
+  localStorage.setItem("to", to);
+  const criptoGuardada = localStorage.getItem("from");
+  const fiatGuardada = localStorage.getItem("to");
+  if (criptoGuardada) {
+    document.getElementById("from").value = criptoGuardada;    
+    console.log("la Criptomoneda guardada es: " + criptoGuardada);
+  }
+  if (fiatGuardada) {
+    document.getElementById("to").value = fiatGuardada;
+    console.log("La moneda FIAT guardada es : " + fiatGuardada);
+  }
+  // Tasas de intercambio de Cripto a FIAT
+ const rates = {
+      BTC: {USD: 30000, COP: 150000000, PEN: 107000, CLP: 24000000},
+      USDT: {USD: 1.0001, COP: 4500, PEN: 3.71, CLP: 807},
+  }
+  
+    
+  // Cálculo del total de la conversión incluyendo comisión
+  const exchanged = cantidad * rates[from][to];
+
+
+  // mostrar los resultados
+ document.getElementById("result").textContent = `${cantidad} ${from} = ${exchanged.toFixed(2)} ${to}`;
 }
 
-let mayorDeEdad = mayorQue(17); 
+function comisionAplicada(){
+  const cantidad = parseFloat(document.getElementById("cantidad").value);
+  const from = document.getElementById("from").value;
+  const to = document.getElementById("to").value;
+  const comisionBaja = 0.01;
+  const comisionAlta = 0.02;
+  
+  const rates = {
+      BTC: {USD: 30000, COP: 150000000, PEN: 107000, CLP: 24000000},
+      USDT: {USD: 1.0001, COP: 4500, PEN: 3.71, CLP: 807},
+      }
+  const exchanged = cantidad * rates[from][to];  
+  console.log("La cantidad base para comision es: " + exchanged);
+  let valorComision = exchanged * comisionAlta;
+  let valorTotal = exchanged + valorComision;
+  
+  
+   if (from === "BTC" && cantidad > 5 || from === "USDT" && cantidad > 150000) {
+      valorComision = exchanged * comisionBaja;
+      valorTotal = exchanged +(exchanged * comisionBaja);
+      console.log("Tienes derecho a un descuento, el valor total de la comisión es: " + valorComision);
+      
+   } else {
+      
+      console.log("No tienes derecho a descuento, el valor de la comisión es: " + valorComision);
+  }
+  
+  document.getElementById("resultadoComision").textContent = `${"Comisión"} = ${valorComision.toFixed(2)} ${to}`
 
-let numero = parseInt(prompt(" Qué edad tienes? "));
-let resultadoEdad = mayorDeEdad(numero);
+  document.getElementById("totalConComision").textContent = `${"Valor Total"} = ${valorTotal.toFixed(2)} ${to}`
 
-if (resultadoEdad) {
-    console.log("El usuario declaró tener " + numero + " años");
-
-function multiplicarNumeros(solicitud,TRM){
-          
-    let resultado = solicitud * TRM;
-    return resultado;
-        
-    }
-
-function Divisas(id, nombre, tasa){
-        this.id = id;
-        this.nombre = nombre;
-        this.tasa = parseFloat(tasa);
 
 }
 
-let todasLasDivisas =[
-    new Divisas("USD", "Dólar Americano", 30500),
-    new Divisas("COP", "Peso Colombiano", 130000000),
-    new Divisas("PEN", "Sol Peruano", 115000),
-    new Divisas("CLP", "Peso Chileno", 24050000),
-];
-let divisasDisponibles = todasLasDivisas.map((todasLasDivisas) => todasLasDivisas.nombre);
-alert("Las divisas disponibles para intercambiar por BTC son: " + divisasDisponibles.join(", "));
 
-let tasaAplicada =  todasLasDivisas[
-    prompt("¿Cuál moneda quieres intercambiar por BTC? Escribe: 0 para USD, 1 para COP, 2 para PEN o 3 para CLP")
-]
-alert("Escogiste " + tasaAplicada.nombre);
-console.log("Los datos de la divisa seleccionada son " , tasaAplicada);
-    
-    
-    let solicitud = parseInt(prompt("Ingresa la cantidad de BTC que quieres vender por " + tasaAplicada.nombre + " ?"));
-        let fee1 = 0.02;
-        let fee2 = 0.01;
-        let TRM = tasaAplicada.tasa;
-        let resultado = multiplicarNumeros(solicitud, TRM);
-        let comision = resultado * fee2;
-        if (solicitud > 5) {
-            comision = resultado * fee2;
-            console.log(" Puedes obtener un descuento en la comisión, tu comisión es " + fee2)
-            alert("Has obtenido un descuento en la comisión.  Pasó del 2% al 1%")
-        } else{
-            comision = resultado * fee1;
-            console.log("Solo ventas superiores a 5 BTC acceden a descuento en comisión")
-            alert(" Vende más de 5 BTC y accede a un descuento del 50% en la comisión")
-        }
-        let total = resultado + comision;
-        multiplicarNumeros(solicitud,TRM); 
-        console.log("La cotización es por: " + tasaAplicada.id + " " + solicitud * TRM);
-        console.log("La comisión es de: " + tasaAplicada.id + " " + comision);
-        console.log("El total con comisión es de: " + tasaAplicada.id + " " + total);
-        alert("El total a pagar incluyendo comisión es de: " + tasaAplicada.id +" " + total);
-            
-           let repetir = prompt("¿Desea otra cotización? Responda Si o No");
-           
-           if (repetir.toUpperCase() == "NO") {
-            let deseaCotizacion = prompt("¿Quieres enviar la cotización a tu correo? responde Si o No");
-                    
-            if (deseaCotizacion.toUpperCase() == "SI") {
-    
-    
-                function Cotizacion (nombres, apellidos, cedula, email, total){
-                    this.nombres = nombres,
-                    this.apellidos = apellidos,
-                    this.cedula = cedula,
-                    this.email = email,
-                    this.total = total
-                }
-                
-                    let cotizacionUno = new Cotizacion(
-                        prompt("¿Cuáles son tus nombres?"),
-                        prompt("¿Cuáles son tus apellidos?"),
-                        prompt("¿Cuál es tu cédula?"),
-                        prompt ("Cuál es tu e-mail"),
-                        total
-                        
-                    )
-                    
-                    let nuevoCliente = Object.values(cotizacionUno);
-                    let nuevoClienteBD = nuevoCliente.join(";");
-                    console.log("Los datos de la cotización son: " , cotizacionUno);
-                    console.log("El nuevo cliente es: " + nuevoClienteBD);
-          
-                    let fechaCotizacion = new Date();
-                    alert("Correo enviado con cotización para venta de " + solicitud + " BTC por valor total de " + tasaAplicada.id + " " + total + " con fecha: " + fechaCotizacion + " tienes 24 horas para ejecutar la transacción");
-    
-            } else {
-            alert("Gracias por cotizar con nosotros");
-    }
-            }
-            else{
-                while (repetir.toUpperCase() == "SI"){ 
-                    function multiplicarNumeros(solicitud,TRM){
-          
-                        let resultado = solicitud * TRM;
-                        return resultado;
-                            
-                        }
-                    
-                    function Divisas(id, nombre, tasa){
-                            this.id = id;
-                            this.nombre = nombre;
-                            this.tasa = parseFloat(tasa);
-                    
-                    }
-                    
-                    let todasLasDivisas =[
-                        new Divisas("USD", "Dolar", 30500),
-                        new Divisas("COP", "Peso Colombiano", 130000000),
-                        new Divisas("PEN", "Sol Peruano", 115000),
-                        new Divisas("CLP", "Peso Chileno", 24050000),
-                    ];
-                    let tasaAplicada =  todasLasDivisas[
-                        prompt("¿Cuál moneda quieres intercambiar por BTC? Escribe: 0 para USD, 1 para COP, 2 para PEN o 3 para CLP")
-                    ]
-                    alert("Escogiste " + tasaAplicada.nombre);
-                    console.log("Los datos de la divisa seleccionada son " , tasaAplicada);
-                    
-                    
-                    let solicitud = parseInt(prompt("Ingresa la cantidad de BTC que quieres vender por " + tasaAplicada.nombre + " ?"));
-                        let fee1 = 0.02;
-                        let fee2 = 0.01;
-                        let TRM = tasaAplicada.tasa;
-                        let resultado = multiplicarNumeros(solicitud, TRM);
-                        let comision = resultado * fee2;
-                        if (solicitud > 5) {
-                            comision = resultado * fee2;
-                            console.log(" Puedes obtener un descuento en la comisión, tu comisión es " + fee2)
-                            alert("Has obtenido un descuento en la comisión.  Pasó del 2% al 1%")
-                        } else{
-                            comision = resultado * fee1;
-                            console.log("Solo ventas superiores a 5 BTC acceden a descuento en comisión")
-                            alert(" Vende más de 5 BTC y accede a un descuento del 50% en la comisión")
-                        }
-                        let total = resultado + comision;
-                        multiplicarNumeros(solicitud,TRM); 
-                        console.log("La cotización es por: " + tasaAplicada.id + " " + solicitud * TRM);
-                        console.log("La comisión es de: " + tasaAplicada.id + " " + comision);
-                        console.log("El total con comisión es de: " + tasaAplicada.id + " " + total);
-                        alert("El total a pagar incluyendo comisión es de: " + tasaAplicada.id +" " + total);
+// Variable para llamar el pop up
+const modal = document.getElementById("modal");
+const modalContent = document.getElementById("modal-content");
 
-                    repetir = prompt("¿Desea otra cotización? Responda Si o No");
+// Botón que abre el pop up
+const btn = document.getElementById("open-modal");
 
-                    if (repetir.toUpperCase() == "NO") {
-                        let deseaCotizacion = prompt("¿Quieres enviar la última cotización a tu correo? responde Si o No");
-                    
-                        if (deseaCotizacion.toUpperCase() == "SI") {
-    
-    
-                        function Cotizacion (nombres, apellidos, cedula, email, total){
-                            this.nombres = nombres,
-                            this.apellidos = apellidos,
-                            this.cedula = cedula,
-                            this.email = email,
-                            this.total = total
-                        }
-                
-                    let cotizacionUno = new Cotizacion(
-                        prompt("¿Cuáles son tus nombres?"),
-                        prompt("¿Cuáles son tus apellidos?"),
-                        prompt("¿Cuál es tu cédula?"),
-                        prompt ("Cuál es tu e-mail"),
-                        total
-                        
-                    )
-                    
-                    let nuevoCliente = Object.entries(cotizacionUno);
-                    let nuevoClienteBD = nuevoCliente.join(";");
-                    console.log("Los datos de la cotización son: " , cotizacionUno);
-                    console.log("El nuevo cliente es: " + nuevoClienteBD);
-          
-                    let fechaCotizacion = new Date();
-                    alert("Correo enviado con cotización para venta de " + solicitud + " BTC por valor total de " + tasaAplicada.id + " " + total + " con fecha: " + fechaCotizacion + " tienes 24 horas para ejecutar la transacción");
-                           
-                        endLoop;
-                        
-                    }}
-    
-                   
-            }}
-} else {
-    alert("Debes ser mayor de edad para vender en nuestro sitio ")
-    
+// Almacenar los botones Enviar y Salir
+const enviarBtn = document.getElementById("Enviar");
+const salirBtn = document.getElementById("Salir");
+
+// Cuando el usuario da click en el botón, aparece el pop up
+btn.onclick = function() {
+  modal.style.display = "block";
+  document.getElementById("cotizacionEnviada").hidden = true;
+}
+
+// Cuando el usuario da click por fuera del pop up, este se cierra
+window.onclick = function(event) {
+if (event.target == modal) {
+  modal.style.display = "none";
+  
+}
+}
+
+// Cuando el usuario da click en el botón enviar
+enviarBtn.onclick = function() {
+
+document.getElementById("modalContent").hidden = true;
+document.getElementById("cotizacionEnviada").hidden = false
+
+function cliente (nombres, email, cedula){
+  this.nombres = nombres,
+  this.email = email,
+  this.cedula = cedula
+  
+  }
+
+ 
+      const inputNombre = document.getElementById("nombre");
+      const inputEmail = document.getElementById("email");
+      const inputCedula = document.getElementById("identificacion");
+
+      const nombres = inputNombre.value;
+      const email = inputEmail.value;
+      const cedula = inputCedula.value;
+      const clienteNuevo = new cliente(nombres, email, cedula);
+      console.log("Los datos del cliente nuevo son: " , clienteNuevo);
+      const datosCliente = Object.entries(clienteNuevo);
+      const clienteDB = datosCliente.join(";");
+      console.log("El nuevo cliente agregado en Base de datos es: "+ clienteDB);
+
+  
+}
+
+// Cuando el usuario da click en el botón Salir
+salirBtn.onclick = function() {
+
+modal.style.display = "none";
 }
 
 
 
-
-
-       
+  
     
-
